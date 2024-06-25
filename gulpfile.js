4da74a245e11e01b1cce49703e351164fc11cd6d
Murat Yaşar <dev.muratyasar@gmail.com>
2024-06-25 14:36:05 +0200

    sass partials

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 
 // Create CSS files
 function buildStyles () {
-  return  src('styles.scss')   // relative path to source of sass file
+  return  src('*.scss')   // relative path to source of sass file
           .pipe(sass())       // use the sass function defined up
           .pipe(dest('css'))  // output the css file
 }
 
 // Watch Sass files and recompile CSS after any change
 function watchTask() {
-  watch(['styles.scss'], buildStyles)
+  watch(['*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask);
